ca79b4aa73bb3118385df4f2117feeef1f896de5
maldrek3103 <cpisanu96@gmail.com>
2021-10-03 12:44:04 +0200

    adding comics section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,7 +3,26 @@
 @section('title')Comics @endsection
 
 @section('content')
-<section id="comics">
-            
-</section>
+    <section id="comics">
+        <img src="{{ asset('images/jumbotron.jpg') }}" alt="Jumbotron" class="img-fluid">
+        <div class="dark-bg">
+            <div class="container">
+                <h1>CURRENT SERIES</h1>
+                {{-- Cards section --}}
+                <div class="cards">
+                    @foreach ($comics as $comic)
+                    {{-- Each card --}}
+                        <div class="card">
+                            
+                                {{-- Card image --}}
+                                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                                {{-- Card title --}}
+                                <h4>{{ $comic['series'] }}</h4>
+                            
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
